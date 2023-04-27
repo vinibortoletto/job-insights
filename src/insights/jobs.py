5eca09392e31e21630e2077934acf58aaaa20163
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-27 16:59:13 -0300

    feat: "filter_by_job_type" function

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -63,4 +63,5 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+    filtered_job_list = [job for job in jobs if job["job_type"] == job_type]
+    return filtered_job_list
