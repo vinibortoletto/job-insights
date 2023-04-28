b27fbeea6b512f6c0a377d47404a4906a8f0c4cf
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-27 23:24:05 -0300

    fix: route typo

--- src/flask_app/routes_and_views.py
+++ src/flask_app/routes_and_views.py
@@ -80,7 +80,7 @@ def list_jobs():
     return render_template("list_jobs.jinja2", ctx=ctx)
 
 
-@bp.route("/jobs/<index>")
+@bp.route("/job/<index>")
 def job(index):
     job_list = read("data/jobs.csv")
     job = get_job(job_list, index)
