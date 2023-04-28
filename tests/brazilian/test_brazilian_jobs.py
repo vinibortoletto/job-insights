098f1999d87b4048105df2a671b2f2ff31bafd04
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-04-27 21:16:00 -0300

    test: "read_brazilian_file" function

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,18 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    result = read_brazilian_file(path)
+
+    """ Test if keys in dict are in english """
+    for job in result:
+        assert "title" in job
+        assert "salary" in job
+        assert "type" in job
+
+    """ Test if keys in dict are NOT in portuguese """
+    for job in result:
+        assert "titulo" not in job
+        assert "salario" not in job
+        assert "tipo" not in job
